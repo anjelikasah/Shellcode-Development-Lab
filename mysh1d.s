510d1c830315076f4bd0250227225d717d9c46df
anjelikasah <38975257+anjelikasah@users.noreply.github.com>
2020-10-12 05:47:08 -0400

    Add files via upload

---
+++ mysh1d.s
@@ -0,0 +1,43 @@
+section .text
+  global _start
+    _start:
+      ; Store the argument string on stack
+
+      xor  eax, eax 
+      push eax          ; Use 0 to terminate the string      
+	push "/env"
+      push "/bin"
+	push "/usr"
+      mov  ebx, esp     ; Get the string address
+
+	push "4"
+	push "=123"
+	push "cccc"
+	mov esi, esp
+
+	push eax
+	push "5678"
+	push "bbb="
+	mov edx, esp
+
+	push eax
+	push "1234"
+	push "aaa="
+	mov edi, esp
+
+      ; Construct the argument array argv[]
+      push eax          ; argv[3] = 0
+      push ebx          ; argv[0] points "/bin/bash"
+      mov  ecx, esp     ; Get the address of argv[]
+   
+      ; For environment variable 
+      ;xor  edx, edx     ; No env variables 
+	push esi
+	push edx
+	push edi
+	mov edx, esp
+
+      ; Invoke execve()
+      xor  eax, eax     ; eax = 0x00000000
+      mov   al, 0x0b    ; eax = 0x0000000b
+      int 0x80
